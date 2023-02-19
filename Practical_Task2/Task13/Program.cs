4cf98cd2dcef566afbe9477500e29612a049d10d
Darek <Darek@mail.com>
2023-02-19 09:18:37 +0300

    task15

---
+++ Practical_Task2/Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру числа
+// или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
---
+++ Practical_Task2/Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход в цифту,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 7)
+{
+    if (num <= 5)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
+}
