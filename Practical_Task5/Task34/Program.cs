27d32069ce160e6ae20d352a1226a5f3f443cf88
Darek <Darek@mail.com>
2023-03-04 00:19:08 +0300

    task34

---
+++ Practical_Task5/Task34/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] GetArray(int size, int minvalue, int maxvalue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minvalue, maxvalue + 1);
+    }
+    return result;
+}
+
+int result(int[] el)
+{
+    int sum = 0;
+    for (int i = 0; i < el.Length; i++)
+    {
+        if (el[i] % 2 == 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+
+int[] array = GetArray(5, 100, 999);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(result(array));
