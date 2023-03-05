1133dc2b516ecc51c31c5a3d9e33053f70e5a733
Darek <Darek@mail.com>
2023-03-05 09:03:40 +0300

    task41

---
+++ Practical_Task5/Task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов на нечётных позициях
+int[] GetArray(int size, int minvalue, int maxvalue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minvalue, maxvalue + 1);
+    }
+    return result;
+}
+
+int result(int[] el)
+{
+    int sum = 0;
+    for (int i = 0; i < el.Length; i++)
+    {
+        if (el[i] % 2 == 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+int[] array = GetArray(5, 1, 100);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(result(array));
---
+++ Practical_Task6/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры число М.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[M];
+
+void arraynums(int M)
+{
+    int i;
+    for (i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int array(int[] nums)
+{
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if(nums[i] > 0)
+        count ++;
+    }
+    return count;
+}
+arraynums(M);
+
+
+Console.WriteLine("Чисел больше 0: " + array(nums));
