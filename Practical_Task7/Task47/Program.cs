094ae394806ed19971b7afe7e5e504fa901f5f0b
Darek <Darek@mail.com>
2023-03-10 19:16:12 +0300

    task47

---
+++ Practical_Task7/Task47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив разметор M x N, заполненный
+// случайными вещественными числами.
+Console.WriteLine("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = new Random().NextDouble() * 20 - 10;
+        Console.Write(arr[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
